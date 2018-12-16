5622ca21afeac6f87f217d11747477fe7dc9407c
kmamiit <kmamiit123@gmail.com>
2018-12-16 01:49:30 -0800

    Initial Commit

---
+++ countCards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  countCards
+//
+//  Created by Kyle Mamiit (New) on 12/16/18.
+//  Copyright © 2018 Kyle Mamiit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
